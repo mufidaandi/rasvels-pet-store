d491a6355e64e2aed742465ec9f606fdadc0abe9
Mufida Andi <mufidaandi@gmail.com>
2023-03-09 15:38:13 -0500

    js changes

--- home.js
+++ home.js
@@ -1,4 +1,63 @@
-"use strict";
+let slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  console.log("show slides");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  console.log("slides " + slides[0]);
+
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function enlargeCatAnimation() {
+  $("cat-image").style.width = "110%";
+}
+
+function reduceCatAnimation() {
+  $("cat-image").style.width = "100%";
+}
+
+function enlargeDogAnimation() {
+  $("dog-image").style.width = "110%";
+}
+
+function reduceDogAnimation() {
+  $("dog-image").style.width = "100%";
+}
+
+function enlargeBirdAnimation() {
+  $("bird-image").style.width = "110%";
+}
+
+function reduceBirdAnimation() {
+  $("bird-image").style.width = "100%";
+}
+
+("use strict");
 
 var expandWidth = false;
 
@@ -24,12 +83,11 @@ var hideModal = function () {
   $("myaccount-modal").style.display = "none";
 };
 
-
 function hideBackground() {
   $("content-wrapper").style.filter = "brightness(0.5)";
 }
 
-function unhideBackground() {  
+function unhideBackground() {
   $("content-wrapper").style.filter = "brightness(1.0)";
 }
 
@@ -55,7 +113,6 @@ function setDogActiveCategory() {
   $("supplies").src = "images/dogsupplies.png";
 
   $("category-image").src = "images/doggo.png";
-
 }
 
 function setBirdActiveCategory() {
@@ -68,50 +125,58 @@ function setBirdActiveCategory() {
   $("supplies").src = "images/birdsupplies.png";
 
   $("category-image").src = "images/birdo.png";
-
 }
 
 function userIconOnHover() {
-  $("user-icon").style.filter = 'brightness(1.3)';
+  $("user-icon").style.filter = "brightness(1.3)";
   $("content-wrapper").style.filter = "brightness(0.5)";
 }
 
 function userIconOnLeave() {
-  $("user-icon").style.filter = 'brightness(1)';
+  $("user-icon").style.filter = "brightness(1)";
   $("content-wrapper").style.filter = "brightness(1.0)";
 }
 
 function cartIconOnHover() {
-  $("cart-icon").style.filter = 'brightness(1.5)';
+  $("cart-icon").style.filter = "brightness(1.5)";
 }
 
 function cartIconOnLeave() {
-  $("cart-icon").style.filter = 'brightness(1)';
+  $("cart-icon").style.filter = "brightness(1)";
 }
 
 function loginBtnOnHover() {
-  $("login-btn").style.filter = 'brightness(1.2)';
+  $("login-btn").style.filter = "brightness(1.2)";
 }
 
 function loginBtnOnLeave() {
-  $("login-btn").style.filter = 'brightness(1)';
+  $("login-btn").style.filter = "brightness(1)";
 }
 
-function login(){
-  var storedName = localStorage.getItem('name');
-  var storedPw = localStorage.getItem('pw');
+function login() {
+  var storedName = localStorage.getItem("name");
+  var storedPw = localStorage.getItem("pw");
 
-  var userName = document.getElementById('uname');
-  var userPw = document.getElementById('pword');
+  var userName = document.getElementById("uname");
+  var userPw = document.getElementById("pword");
 
-  if (userName.value == storedName && userPw.value == storedPw){
-      alert('You are logged in.');
-  } else{
-      alert('Error on login');
+  if (userName.value == storedName && userPw.value == storedPw) {
+    alert("You are logged in.");
+  } else {
+    alert("Error on login");
   }
 }
 
 window.onload = function () {
+  showSlides(slideIndex);
+
+  $("cat-image").onmouseover = enlargeCatAnimation;
+  $("cat-image").onmouseleave = reduceCatAnimation;
+  $("dog-image").onmouseover = enlargeDogAnimation;
+  $("dog-image").onmouseleave = reduceDogAnimation;
+  $("bird-image").onmouseover = enlargeBirdAnimation;
+  $("bird-image").onmouseleave = reduceBirdAnimation;
+
   expandSearch();
 
   $("myaccount").onmouseover = userIconOnHover;
