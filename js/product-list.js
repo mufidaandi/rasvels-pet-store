cdbd1638fd530b2633941a9e5dbf80db55d13b52
Jenil Varma <varmajenil123@gmail.com>
2023-03-19 11:52:55 -0400

    - modified CSS for better formatting of the product card - modified JS for filtering products based on the categories and sub-categories - removed unnecessary duplicate functions from products.js

--- js/product-list.js
+++ js/product-list.js
@@ -1,11 +1,34 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+    }
+  }
+  return false;
+};
+
 $(document).ready(function() {
   const productList = $('#product-list');
+  var urlParameter = getUrlParameter('category').split("_");
+  var productCategory = urlParameter[0];
+  var productSubCategory = urlParameter[1];
 
   for (let i = 0; i < products.length; i++) {
-    const product = products[i];
+    const product = products.filter(value => value.category === productCategory && value.subcategory === productSubCategory)[i];
 
     // Create a div for the product card
     const productCard = $('<div class="product-card"></div>');
+
+    // Create a div for the product details
+    const productLinkStart = $('<a>');
+    productLinkStart.attr('href', "product-detail.html?productId="+ product.id)
 
     // Create an image element for the product image
     const productImage = $('<img>');
@@ -18,20 +41,36 @@ $(document).ready(function() {
     // Create a div for the product details
     const productDetails = $('<div class="product-details"></div>');
 
-    // Create an h2 element for the product name
-    const productName = $('<h2 class="product-name"></h2>');
-    productName.text(product.name);
-
     // Create a p element for the product price
     const productPrice = $('<p class="product-price"></p>');
     productPrice.text(`$${product.price}`);
+
+    // Create an h2 element for the product name
+    const productName = $('<p class="product-name"></p>');
+    productName.text(product.name);
 
     // Create a div for the product rating
     const productRating = $('<div class="product-rating"></div>');
 
     // Create a span element for the product rating value
     const productRatingValue = $('<span class="product-rating-value"></span>');
-    productRatingValue.text(Math.floor(Math.random() * 6));
+    const filledStars = Math.floor(Math.random() * 6);
+    productRatingValue.text(filledStars);
+
+    // Create filled stars
+    for (let j = 0; j < filledStars; j++) {
+      const star = $('<i>').addClass('fas fa-star');
+      productRating.append(star);
+    }
+
+    // Calculate number of empty stars
+    const emptyStars = 5 - filledStars;
+
+    // Create empty stars
+    for (let j = 0; j < emptyStars; j++) {
+      const star = $('<i>').addClass('far fa-star');
+      productRating.append(star);
+    }
 
     // Create a span element for the maximum product rating
     const productRatingMax = $('<span class="product-rating-max"></span>');
@@ -42,15 +81,17 @@ $(document).ready(function() {
     productRating.append(productRatingMax);
 
     // Add the product name, price, and rating to the product details
-    productDetails.append(productName);
     productDetails.append(productPrice);
+    productDetails.append(productName);
     productDetails.append(productRating);
 
     // Add the product image and details to the product card
     productCard.append(productImage);
     productCard.append(productDetails);
 
+    productLinkStart.append(productCard);
+
     // Add the product card to the product list
-    productList.append(productCard);
+    productList.append(productLinkStart);
   }
 });
