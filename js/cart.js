7f1944b3f6e7d5b29b50178302be0d043daec8d6
tichvu2611 <tichvu2611@gmail.com>
2023-04-04 18:11:48 -0400

    addItemToCart and getAllCartItems function

--- js/cart.js
+++ js/cart.js
@@ -1,17 +1,36 @@
-const cart = [];
-
-function addItemToCart(userId, item) {
-    const userCart = carts[userId];
-    const existingItem = userCart.find(cartItem => cartItem.id === item.id);
-    if (existingItem) {
-      existingItem.quantity++;
-    } else {
+function addItemToCart(item) {
+  var myJsonData = localStorage.getItem('userCart');
+  var userCart = JSON.parse(myJsonData);
+    if(userCart === null){
+      userCart = [];
       userCart.push({
         id: item.id,
         name: item.name,
         price: item.price,
-        quantity: 1
+        quantity: item.quantity
       });
+    } else {
+        existingItem = userCart.find(cartItem => cartItem.id === item.id);
+      if (existingItem) {
+        existingItem.quantity = Number(existingItem.quantity) + Number(item.quantity);
+      } else {
+        userCart.push({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          quantity: item.quantity
+        });
+      }
     }
+    
+    console.log("Item added");
+    console.log(userCart);
+    localStorage.setItem("userCart", JSON.stringify(userCart));
   }
-  
+
+
+  function getAllCartItems(){
+    var myJsonData = localStorage.getItem('userCart');
+    var userCart = JSON.parse(myJsonData);
+    return userCart;
+  }
