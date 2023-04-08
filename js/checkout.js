16dc69c7fee77020e271c279cbb6a42e85daf03f
tichvu2611 <tichvu2611@gmail.com>
2023-04-07 23:29:36 -0400

    delete items in cart, add some checks

--- js/checkout.js
+++ js/checkout.js
@@ -12,4 +12,18 @@ jQuery(document).ready(function () {
     var totalAmount = Number(subTotal) + Number(deliverCharges);
     jQuery("#subTotal").text('$' + subTotal);
     jQuery("#totalAmount").text('$' + totalAmount);
+
+    // Check if users click on Place Order, system will remove all items in the cart and redirect to thankyou page
+    const myform = document.getElementById("myform");
+    myform.addEventListener("submit", function(event) {
+    // prevent the form from submitting immediately
+    event.preventDefault();
+    
+    // clear all items in the cart
+    localStorage.removeItem("userCart");
+    console.log('All items have removed');
+    // submit the form programmatically after your code is done executing
+    myform.submit();
+    });
 });
+
